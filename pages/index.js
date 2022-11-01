68398ef917c57d3ee7e30a144b95b510cc1d08ff
TheJobGG <jobglez80@gmail.com>
2022-11-01 01:52:25 -0700

    Extracted main component and fetch method

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import styles from '../styles/Home.module.css'
 
 import Layout from '../components/Layout';
+import { getData } from './_app';
+import MainContent from '../components/MainContent';
 
 /* export default function Home({ news, category }) { */
 export default function Home({ response }) {
@@ -13,57 +14,12 @@ export default function Home({ response }) {
 
   return (
     <Layout>
-      <main className={styles.main}>
-        <h2 className={styles.title}>
-          {category == 'all'
-            ? `Most recently news`
-            : `All about ${category}`}
-        </h2>
-        {news.length === 0
-          ? <p>Parece que sucedió un error...</p>
-          :
-          <div className={styles.newsContainer}>
-            {news.map(item => {
-              const { author, content, date, id, imageUrl, time, title } = item
-
-              return <Link
-                href={{
-                  pathname: `/news/${id}`,
-                  query: item
-                }}
-                key={id}
-                className={styles.card}>
-                <Image
-                  src={imageUrl}
-                  alt={'image about ' + title}
-                  width={0}
-                  height={0}
-                  sizes="50vw"
-                />
-                <span className={styles.date}>{date} - {time}</span>
-                <h3>{title}</h3>
-                <p className={styles.description}>{content}</p>
-                <span className={styles.author}>{author}</span>
-              </Link>
-            })}
-          </div>
-        }
-      </main>
+      <MainContent category={category} news={news}/>
     </Layout>
   )
 }
 
 
-export async function getServerSideProps() {
-  const API_URL = 'https://inshorts.deta.dev/news?category=all'
-  let news;
-  const fetchResponse = await fetch(`${API_URL}`)
-  const response = await fetchResponse.json()
-
-
-  return {
-    props: {
-      response
-    }
-  }
+export function getServerSideProps() {
+  return getData('all')
 }
