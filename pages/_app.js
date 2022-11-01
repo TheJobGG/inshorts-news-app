413b6ea2fb2e39e9bf78592ed249a988a8f80d12
TheJobGG <jobglez80@gmail.com>
2022-11-01 01:51:05 -0700

    Extracted fetch method to this file

--- pages/_app.js
+++ pages/_app.js
@@ -5,3 +5,17 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+
+export async function getData(category = 'all') {
+  const API_URL = `https://inshorts.deta.dev/news?category=${category}`
+  let news;
+  const fetchResponse = await fetch(`${API_URL}`)
+  const response = await fetchResponse.json()
+
+  return {
+    props: {
+      response
+    }
+  }
+}
