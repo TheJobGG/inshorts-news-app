ec4ee08c4735a3a9ab639c91b2c31d0ea0c7dfef
TheJobGG <jobglez80@gmail.com>
2022-11-01 02:02:35 -0700

    Add Head component and title tag

--- components/Layout.js
+++ components/Layout.js
@@ -1,30 +1,35 @@
 import Link from "next/link";
 import Image from "next/image"
+import Head from "next/head";
 
 import styles from "../styles/Layout.module.css"
-import Vercel from '../public/vercel.svg'
 
 export default function Layout({ children }) {
     return (
-        <div className={styles.Layout}>
-            <header className={styles.header}>
-                <Link href='/'><h1>Inshorts News</h1></Link>
-                <ul>
-                    <li className={styles.linkDefault}><Link href='/Business'>Business</Link></li>
-                    <li className={styles.linkDefault}><Link href='/Sports'>Sports</Link></li>
-                    <li className={styles.linkDefault}><Link href='/Technology'>Technology</Link></li>
-                    <li className={styles.linkDefault}><Link href='/Startup'>Startup</Link></li>
-                </ul>
-            </header>
-            {children}
-            <footer className={styles.footer}>
-                <p>This app is using</p>
-                <Link
-                    href='https://github.com/cyberboysumanjay/Inshorts-News-API'
-                    target="_blank">
-                    Inshorts News API
-                </Link>
-            </footer>
-        </div>
+        <>
+            <Head>
+                <title>In-shorts News</title>
+            </Head>
+            <div className={styles.Layout}>
+                <header className={styles.header}>
+                    <Link href='/'><h1>Inshorts News</h1></Link>
+                    <ul>
+                        <li className={styles.linkDefault}><Link href='/Business'>Business</Link></li>
+                        <li className={styles.linkDefault}><Link href='/Sports'>Sports</Link></li>
+                        <li className={styles.linkDefault}><Link href='/Technology'>Technology</Link></li>
+                        <li className={styles.linkDefault}><Link href='/Startup'>Startup</Link></li>
+                    </ul>
+                </header>
+                {children}
+                <footer className={styles.footer}>
+                    <p>This app is using</p>
+                    <Link
+                        href='https://github.com/cyberboysumanjay/Inshorts-News-API'
+                        target="_blank">
+                        Inshorts News API
+                    </Link>
+                </footer>
+            </div>
+        </>
     )
 }
