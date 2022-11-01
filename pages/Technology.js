e8ea743f72186f262f42aebde7ec02535ca9a6a8
TheJobGG <jobglez80@gmail.com>
2022-11-01 01:51:38 -0700

    Created rest of sections

---
+++ pages/Technology.js
@@ -0,0 +1,21 @@
+import Layout from "../components/Layout"
+
+import MainContent from "../components/MainContent"
+import { getData } from "./_app";
+
+export default function Technology({ response }) {
+    const category = response.category;
+    const news = response.data;
+    return (
+        <>
+            <Layout>
+            <MainContent category={category} news={news} />
+            </Layout>
+        </>
+    )
+}
+
+
+export function getServerSideProps() {
+    return getData('technology')
+}
