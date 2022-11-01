b5fcd60d4987f00c8d33debc55c1849f7f5f1723
TheJobGG <jobglez80@gmail.com>
2022-11-01 01:50:03 -0700

    Added Link to go to section

--- components/Layout.js
+++ components/Layout.js
@@ -6,26 +6,25 @@ import Vercel from '../public/vercel.svg'
 
 export default function Layout({ children }) {
     return (
-        <>
+        <div className={styles.Layout}>
             <header className={styles.header}>
                 <Link href='/'><h1>Inshorts News</h1></Link>
-                {/* <ul>
-                    <li>All</li>
-                    <li>Business</li>
-                    <li>Sport</li>
-                    <li>Technology</li>
-                    <li>Startup</li>
-                </ul> */}
+                <ul>
+                    <li className={styles.linkDefault}><Link href='/Business'>Business</Link></li>
+                    <li className={styles.linkDefault}><Link href='/Sports'>Sports</Link></li>
+                    <li className={styles.linkDefault}><Link href='/Technology'>Technology</Link></li>
+                    <li className={styles.linkDefault}><Link href='/Startup'>Startup</Link></li>
+                </ul>
             </header>
             {children}
             <footer className={styles.footer}>
-                <p>This app is using the </p>
+                <p>This app is using</p>
                 <Link
                     href='https://github.com/cyberboysumanjay/Inshorts-News-API'
                     target="_blank">
                     Inshorts News API
                 </Link>
             </footer>
-        </>
+        </div>
     )
 }
